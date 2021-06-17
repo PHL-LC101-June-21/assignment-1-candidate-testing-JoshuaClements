0d09b585cb37e584f2fb44b2485a79f747c48a31
Josh Clements <85710890+JoshuaClements@users.noreply.github.com>
2021-06-17 18:22:50 +0000

    Added questions & input

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,9 +1,8 @@
-const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-
+const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " "; 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -16,19 +15,22 @@ let candidateAnswers;
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
-}
+const candidateName = input.question("What is your name?");}
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+const answer = input.question("Who was the first American woman in space? ");
+const correctAnswer = "Sally Ride"
+const candidateAnswer = " "
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
+if (candidateAnswer = "Sally Ride") {
+ console.log("Correct Answer!");
+}
   let grade;
   
 
